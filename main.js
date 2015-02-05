8ca34da94b33a083f3ef317fb0bcc91395fa4ac8
Alon Dahari <climbinghobo@gmail.com>
2015-02-05 10:06:17 -0700

    add dashInsert()

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ var palindrome = function(str){
 	return str.split('').reverse().join('') === str;
 	
 };
+
+var dashInsert = function(num){
+	var arr = num.toString().split('');
+	return arr.map(function (val, i) {
+		if ( (val % 2 != 0) && arr[i+1] && (arr[i+1] % 2 != 0)  ) {
+			return val + '-';
+		} else {
+			return val;
+		}
+	}).join('');
+};
--- test/spec.js
+++ test/spec.js
@@ -6,4 +6,11 @@ describe('Problem set 4', function() {
 	  });
 		
 	});
+	describe("dashInsert", function() {
+	  it('should add dashes between adjacent odd numbers', function() {
+	    expect(dashInsert(454793)).toEqual("4547-9-3");
+	  });
+		
+	});
+
 });
